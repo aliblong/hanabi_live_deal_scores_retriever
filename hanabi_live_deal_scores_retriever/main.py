52535398f84b711fb2155b1963fe8e2a3bfc1198
Aaron Liblong <liblonga@hotmail.com>
2021-04-20 10:33:14 -0400

    feat: remove csv output support
    Before hanabi-competitions.com existed, results were collected in a
    Google Sheet, so I output them in csv with this tool.
    Now, there is no foreseen use for a csv output; the site uses json.

--- hanabi_live_deal_scores_retriever/main.py
+++ hanabi_live_deal_scores_retriever/main.py
@@ -1,7 +1,6 @@
 __version__ = '0.1.0'
 
 import argparse
-import csv
 import json
 import logging
 import os
@@ -47,14 +46,9 @@ def parse_args():
         required=True,
         help="ID of the variant, according to hanabi.live, e.g. 106, for Pink(6 suits)",
     )
-    output_group = parser.add_mutually_exclusive_group(required=True)
-    output_group.add_argument(
+    parser.add_argument(
         "--output_json_file_path",
         "-j",
-    )
-    output_group.add_argument(
-        "--output_csv_file_path",
-        "-c",
     )
     return parser.parse_args()
 
@@ -67,62 +61,26 @@ def main():
     args = parse_args()
     logging.basicConfig(level=args.verbosity)
     bot = Bot(os.getenv("hanabi_live_username"), os.getenv("hanabi_live_password"))
-    teams_results = {}
-    team_captains = {}
     seed_results = []
-    base_seed_names = [f"{SEED_PREFIX}{args.date}-{idx + 1}" for idx in range(args.num_seeds)]
-    for deal_idx, base_seed_name in enumerate(base_seed_names):
+    # Seed names are 1-indexed
+    for seed_idx in range(1, args.num_seeds + 1):
+        base_seed_name = f"{SEED_PREFIX}{args.date}-{seed_idx}"
         seed_name = f"p{args.num_players}v{args.variant_id}s{base_seed_name}"
         results = bot.get_deal_scores(seed_name)
-        if args.output_csv_file_path:
-            # Veto all games where not every player is on their first attempt at the deal
-            past_participants = set()
-            # Results were previously in order of ascending game ID, i.e. game completion datetime.
-            # Then, they were changed to be in order of _de_scending game ID.
-            # Let's just try to rely on consistency of server behaviour as little as possible.
-            for game_id, properties in sorted(results.items()):
-                players = set(properties["players"])
-                # if the intersection is an empty set, everyone is on their first attempt
-                if not players.intersection(past_participants):
-                    players_tuple = tuple(sorted(players))
-                    if players_tuple not in teams_results:
-                        teams_results[players_tuple] = [(None, None, None, None, None)] * args.num_seeds
-                        team_captains[players_tuple] = properties["players"][0]
-                    score = properties["score"]
-                    turns = properties["turns"]
-                    datetime_started = properties["datetime_started"]
-                    datetime_ended = properties["datetime_ended"]
-                    teams_results[players_tuple][deal_idx] = (
-                        game_id, score, turns, datetime_started, datetime_ended)
-                past_participants.update(players)
-        else:
-            seed_results.append({
-                "base_seed_name": base_seed_name,
-                "games": [
-                    dict({"game_id": game_id}, **game_results)
-                    for game_id, game_results in results.items()
-                ],
-            })
 
-    if args.output_csv_file_path:
-        with open(args.output_csv_file_path, 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            for team, games_results in teams_results.items():
-                flattened_games_results = [
-                    game_subresult
-                    for game_result in games_results
-                    for game_subresult in game_result[:3]
-                ]
-                team_captain = team_captains[team]
-                team_with_captain_first = [team_captain] \
-                    + [player for player in team if player != team_captain]
-                writer.writerow(team_with_captain_first + flattened_games_results)
-    else:
-        output = {
-            "num_players": args.num_players,
-            "variant_id": args.variant_id,
-            "end_date": args.date,
-            "seeds_games": seed_results,
-        }
-        with open(args.output_json_file_path, 'w') as json_file:
-            json_file.write(json.dumps(output))
+        seed_results.append({
+            "base_seed_name": base_seed_name,
+            "games": [
+                dict({"game_id": game_id}, **game_results)
+                for game_id, game_results in results.items()
+            ],
+        })
+
+    output = {
+        "num_players": args.num_players,
+        "variant_id": args.variant_id,
+        "end_date": args.date,
+        "seeds_games": seed_results,
+    }
+    with open(args.output_json_file_path, 'w') as json_file:
+        json_file.write(json.dumps(output))
